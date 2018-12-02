b3fa2d24d918ed08704f4aa899f453a5a15eb2a4
William Calderipe <wcalderipe@gmail.com>
2018-12-03 09:19:36 +1100

    Add test case to ensure evaluation with AND logical operator

--- api/test/unit/domain/evaluate.test.ts
+++ api/test/unit/domain/evaluate.test.ts
@@ -9,7 +9,7 @@ describe('evaluate', () => {
     name: 'newLoginForm'
   }
 
-  test('returns true when given is present in context', () => {
+  test('returns true when context is present in the criteria', () => {
     const context: Context = {
       country: 'BR'
     }
@@ -17,11 +17,39 @@ describe('evaluate', () => {
     expect(evaluate(feature, context)).toEqual(true)
   })
 
-  test('returns false when given is not present in context', () => {
+  test('returns false when context is not present in the criteria', () => {
     const context: Context = {
       country: 'US'
     }
 
     expect(evaluate(feature, context)).toEqual(false)
+  })
+
+  describe('AND logical operator', () => {
+    const feature: Feature = {
+      criteria: {
+        fruit: ['apple', 'strawberry'],
+        vegetable: ['potato', 'tomatoe']
+      },
+      name: 'fooFeature'
+    }
+
+    test('returns false when context don\'t match all criterias', () => {
+      const context: Context = {
+        fruit: 'apple',
+        vegetable: 'garlic'
+      }
+
+      expect(evaluate(feature, context)).toEqual(false)
+    })
+
+    test('returns true when context match all criterias', () => {
+      const context: Context = {
+        fruit: 'apple',
+        vegetable: 'potato'
+      }
+
+      expect(evaluate(feature, context)).toEqual(true)
+    })
   })
 })
