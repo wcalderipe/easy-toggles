3931a594eb23ab66221f7a67200703301534d141
William Calderipe <wcalderipe@gmail.com>
2018-12-13 22:36:17 +1100

    Create input types for update mutation

--- api/src/app.ts
+++ api/src/app.ts
@@ -36,24 +36,39 @@ const buildApolloServer = (store: Store): ApolloServer => {
       application(id: ID!): Application
     }
 
-    input ApplicationInput {
+    input CreateApplicationInput {
       name: String!
-      features: [FeatureInput]!
+      features: [CreateFeatureInput]!
     }
 
-    input FeatureInput {
+    input CreateFeatureInput {
       name: String!
-      criterias: [CriteriaInput]!
+      criterias: [CreateCriteriaInput]!
     }
 
-    input CriteriaInput {
+    input CreateCriteriaInput {
+      name: String!
+      values: [String]!
+    }
+
+    input UpdateApplicationInput {
+      name: String!
+      features: [UpdateFeatureInput]!
+    }
+
+    input UpdateFeatureInput {
+      name: String!
+      criterias: [UpdateCriteriaInput]!
+    }
+
+    input UpdateCriteriaInput {
       name: String!
       values: [String]!
     }
 
     type Mutation {
-      createApplication(input: ApplicationInput): Application
-      updateApplication(id: ID!, input: ApplicationInput): Application
+      createApplication(input: CreateApplicationInput): Application
+      updateApplication(id: ID!, input: UpdateApplicationInput): Application
       deleteApplication(id: ID!): Boolean
     }
   `
